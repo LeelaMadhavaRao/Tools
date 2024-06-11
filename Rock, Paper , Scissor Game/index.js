3b093a2cf6978dd507b70407d0ad54ec0b4df7b9
LeelaMadhavaRao <leelamadhav.nulakani@gmail>
2024-06-12 00:27:27 +0530

    draw case in js

--- Rock, Paper , Scissor Game/index.js
+++ Rock, Paper , Scissor Game/index.js
@@ -1,21 +1,50 @@
 var SelectedChoice = null;
+var score=document.getElementById("score");
+var gobutton=document.getElementById("go_button");
+var third=document.getElementById("img");
+var but1round=document.getElementById("but1round").textContent="--";
+var but2round=document.getElementById("but2round").textContent="--";
+function choice(val){
+    SelectedChoice=val;
+}
 function ResetStats(){
 
 }
 function ComputerChoice(){
-    return Math.floor(Math.random() * 4);
+    return Math.floor(Math.random() * 3);
 }
 function SelectChoice(item){
 
 }
 function Go(){
-    if(SelectChoice == null){
+    if(SelectedChoice == null){
         alert("Please Select a Choice!");
         return;
     }
     Computer_Choice = ComputerChoice();
-    //Render Computer Choice Animation
-
+    if((Computer_Choice ==0 && SelectedChoice==0) ||(Computer_Choice ==2 && SelectedChoice==2) || (Computer_Choice ==1 && SelectedChoice==1))
+        {
+            var result=document.getElementById("result");
+            result.textContent="Draw";
+            gobutton.textContent="Next round";
+            if(Computer_Choice ==0)
+                {
+                    img.src="./../Img/rock.jpg";
+                }
+            else if(Computer_Choice ==1)
+                {
+                    img.src="./../Img/paper.jpg";
+                }
+            else if(Computer_Choice ==2)
+                {
+                    img.src="./../Img/scissor.jpg";
+                }
+                but2round.textContent=but1round.textContent;
+                but1round.textContent="--";
+                SelectedChoice="null";
+        }
+    
+    
 
     //Display Result
 
@@ -23,7 +52,7 @@ function Go(){
     //Update Stats
 
 
-    //Change Button from 'Go' to Start New Game
+    //Change Button from 'Go' to Start Next round
 }
 function NewGame(){
     SelectChoice = null;
