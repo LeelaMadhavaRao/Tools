49f970978638910786642abb54632501eff865d4
LeelaMadhavaRao <leelamadhav.nulakani@gmail>
2024-06-15 00:26:14 +0530

    changed

--- Quiz/index.js
+++ Quiz/index.js
@@ -1,62 +1,55 @@
 var Questions = [
-    {'Question':' What is the chemical symbol for gold?','options':['Au','Ag','Pb','Fe'],'answer':'Au'},
+    {'Question':'What is the chemical symbol for gold?','options':['Au','Ag','Pb','Fe'],'answer':'Au'},
     {'Question':'Who wrote the play "Romeo and Juliet"?','options':['William Shakespeare','Charles Dickens','Mark Twain','Jane Austen'],'answer':'William Shakespeare'},
-    {'Question':'What is the capital city of Canada?','options':[' Toronto','Vancouver','Ottawa','Montreal'],'answer':' Ottawa'},
+    {'Question':'What is the capital city of Canada?','options':['Toronto','Vancouver','Ottawa','Montreal'],'answer':'Ottawa'},
     {'Question':'What is the value of π (pi) up to two decimal places?','options':['3.12','3.14','3.16','3.18'],'answer':'3.14'},
-    {'Question':'Who was the first President of the United States?','options':['Thomas Jefferson','Abraham Lincoln','George Washington','John Adams'],'answer':'George Washington'},
-    {'Question':'Question 6','options':['optiona','optionb','optionc','optiond'],'answer':'optiona'},
-]
+    {'Question':'Who was the first President of the United States?','options':['Thomas Jefferson','Abraham Lincoln','George Washington','John Adams'],'answer':'George Washington'}
+];
+
 var CurrentQuestion = 0;
 var Score = 0;
 
 function RenderQuestion(id){
-    id = Number(id);
-    if(id === Questions.length - 1){
-        var html=" ";
-        html= `<h4 style="font-weight: bolder; color: green;">Score : ${Score}</h4>`;
-        document.getElementById('score').innerHTML=html;
+    if(id === Questions.length){
+        var html = `<h4 style="font-weight: bolder; color: green;">Score: ${Score}</h4>`;
+        document.getElementById('score').innerHTML = html;
         return;
     }
-    document.getElementById('a').checked=false;
-    document.getElementById('b').checked=false;
-    document.getElementById('c').checked=false;
-    document.getElementById('d').checked=false;
-    document.getElementById('QuestionNo').innerHTML = id+1;
+
+    document.getElementById('a').checked = false;
+    document.getElementById('b').checked = false;
+    document.getElementById('c').checked = false;
+    document.getElementById('d').checked = false;
+
+    document.getElementById('QuestionNo').innerHTML = id + 1;
     document.getElementById('Question').innerHTML = Questions[id].Question;
     document.getElementById('Optiona').innerHTML = Questions[id].options[0];
     document.getElementById('Optionb').innerHTML = Questions[id].options[1];
     document.getElementById('Optionc').innerHTML = Questions[id].options[2];
     document.getElementById('Optiond').innerHTML = Questions[id].options[3];
-    if(document.getElementById('a').checked==true)
-        {
-            if(Questions[id].options[0]==Questions[id].answer)
-                ++Score;
-        }
-    if(document.getElementById('a').checked==true)
-        {
-            if(Questions[id].options[0]==Questions[id].answer)
-                ++Score;
-        }
-    if(document.getElementById('a').checked==true)
-        {
-            if(Questions[id].options[0]==Questions[id].answer)
-                ++Score;
-        }
-    if(document.getElementById('a').checked==true)
-        {
-            if(Questions[id].options[0]==Questions[id].answer)
-                ++Score;
-        }
-    if(id === Questions.length - 2){
+
+    if(id === Questions.length - 1){
         document.getElementById('Next_Button').innerHTML = 'Submit';
+    } else {
+        document.getElementById('Next_Button').innerHTML = 'Next';
     }
-    
-    CurrentQuestion += 1;
+
+    CurrentQuestion = id;
     
 }
+
 function NextQuestion(){
-    RenderQuestion(CurrentQuestion);
+    var selectedOption = document.querySelector('input[name="QuizApp"]:checked');
+    if (selectedOption) {
+        var answer = selectedOption.nextElementSibling.innerHTML.trim();
+        if (answer === Questions[CurrentQuestion].answer) {
+            Score++;
+            
+        }
+    }
+    RenderQuestion(CurrentQuestion + 1);
 }
-setTimeout(()=>{
+
+setTimeout(() => {
     RenderQuestion(0);
-},300)
+}, 300);
